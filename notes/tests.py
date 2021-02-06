95b41c803cdf3983315d165bbbe12a92be57bfa0
junior451 <kwabenaedwards@gmail.com>
2021-02-06 19:26:00 +0000

    initial test set up

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+  def test_it_returns_the_correct_content(self):
+    """
+    it shows the correct message on the index page
+    """
+
+    response = self.client.get('/notes/')
+    self.assertContains(response, "Hello World, You are at the notes index page")
+    self.assertEqual(response.status_code, 200)
+  
+  
